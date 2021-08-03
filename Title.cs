1a2f4f1acb072c64ce9606e2e64a5b50959a0ce9
zLynnx06 <86571756+zLynnx06@users.noreply.github.com>
2021-08-02 22:54:54 -0500

    Create Title.cs

---
+++ Title.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace NitroGen
+{
+    class Title
+    {
+        public static string asciiTitle = @"
+███╗   ██╗██╗████████╗██████╗  ██████╗      ██████╗ ███████╗███╗   ██╗
+████╗  ██║██║╚══██╔══╝██╔══██╗██╔═══██╗    ██╔════╝ ██╔════╝████╗  ██║
+██╔██╗ ██║██║   ██║   ██████╔╝██║   ██║    ██║  ███╗█████╗  ██╔██╗ ██║
+██║╚██╗██║██║   ██║   ██╔══██╗██║   ██║    ██║   ██║██╔══╝  ██║╚██╗██║
+██║ ╚████║██║   ██║   ██║  ██║╚██████╔╝    ╚██████╔╝███████╗██║ ╚████║
+╚═╝  ╚═══╝╚═╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝      ╚═════╝ ╚══════╝╚═╝  ╚═══╝
+===> A simple Discord Nitro gift code generator <===
+";
+    }
+}
